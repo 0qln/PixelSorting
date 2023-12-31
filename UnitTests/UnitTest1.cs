f6a7bfb9701cb428f6b5cdeeab7001c26e87a55e
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-12-31 16:34:01 +0100

    Add unittest

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+    }
+}
