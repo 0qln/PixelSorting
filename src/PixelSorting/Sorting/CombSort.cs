0d4d87d6f9e1b02f999ece4db159dd6b5939b937
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-01 21:29:56 +0200

    Implement comb sort

---
+++ src/PixelSorting/Sorting/CombSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public partial class Sorter<TPixel>
+        where TPixel : struct
+    {
+        public static void CombSort(PixelSpan2D span, IComparer<TPixel> comparer, int pureness)
+        {
+            int gap = span.ItemCount;
+            float shrink = 1.3f;
+            bool sorted = false;
+
+            for (int p = 0; p < pureness && !sorted; p++)
+            {
+                // Update the gap value for a next comb
+                gap = (int)(gap / shrink);
+                if (gap <= 1)
+                {
+                    gap = 1;
+                    sorted = true;
+                }
+                //else if (gap == 9 || gap == 10)
+                //{
+                //    gap = 11;
+                //}
+
+                for (int i = 0; i + gap < span.ItemCount; i++)
+                {
+                    if (comparer.Compare(span[i], span[i + gap]) > 0)
+                    {
+                        Swap(span, i, i + gap);
+                        sorted = false;
+                    }
+                }
+            }
+        }
+
+        public static void CombSort(PixelSpan span, IComparer<TPixel> comparer)
+        {
+            int gap = span.ItemCount;
+            float shrink = 1.3f;
+            int maxIterations = 1000;
+            bool sorted = false;
+
+            for (int pureness = 0; pureness < maxIterations && !sorted; pureness++)
+            {
+                // Update the gap value for a next comb
+                gap = (int)(gap / shrink);
+                if (gap <= 1)
+                {
+                    gap = 1;
+                    sorted = true;
+                }
+                //else if (gap == 9 || gap == 10)
+                //{
+                //    gap = 11;
+                //}
+
+                for (int i = 0; i + gap < span.ItemCount; i++)
+                {
+                    if (comparer.Compare(span[i], span[i + gap]) > 0)
+                    {
+                        Swap(span, i, i + gap);
+                        sorted = false;
+                    }
+                }
+            }
+        }
+    }
+}
