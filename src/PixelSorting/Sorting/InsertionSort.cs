18ed5ecf4506f54c89e013d1b4d085fb17cc9005
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-02 23:59:02 +0200

    Update InsertionSort.cs

--- src/PixelSorting/Sorting/InsertionSort.cs
+++ src/PixelSorting/Sorting/InsertionSort.cs
@@ -56,6 +56,28 @@ namespace Sorting
         /// </summary>
         /// <param name="span"></param>
         /// <param name="comparer"></param>
+        public static void InsertionSort(PixelSpan2D span, IComparer<TPixel> comparer)
+        {
+            for (int i = 0; i < span.ItemCount - 1; ++i)
+            {
+                TPixel t = span[i + 1];
+
+                int j = i;
+                while (j >= 0 && comparer.Compare(t, span[j]) < 0)
+                {
+                    span[j + 1] = span[j];
+                    --j;
+                }
+
+                span[j + 1] = t;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="comparer"></param>
         /// <param name="lo">Inclusive</param>
         /// <param name="hi">Inclusive</param>
         public static void InsertionSort(PixelSpan2D span, IComparer<TPixel> comparer, int lo, int hi)
