1e6416aca0ffe08a6fdac2dbf0585a11da43718d
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-02 23:58:51 +0200

    Create RadixSort.cs

---
+++ src/PixelSorting/Sorting/RadixSort.cs
@@ -0,0 +1,27 @@
+﻿using Sorting.Pixels.KeySelector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    // TODO?
+
+    public partial class Sorter<TPixel>
+        where TPixel : struct
+    {
+        public enum RadixOrder { MSD, LSD }
+
+        public static void RadixSortBase10(PixelSpan2D span, IOrderedKeySelector<TPixel> selector, RadixOrder order)
+        {
+            if (order == RadixOrder.LSD)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+}
