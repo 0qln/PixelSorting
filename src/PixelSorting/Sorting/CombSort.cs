e20cb0d1552bdadf6b2a679a9de8ad93cb6bafbf
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-05-02 23:59:17 +0200

    Update CombSort.cs

--- src/PixelSorting/Sorting/CombSort.cs
+++ src/PixelSorting/Sorting/CombSort.cs
@@ -9,6 +9,37 @@ namespace Sorting
     public partial class Sorter<TPixel>
         where TPixel : struct
     {
+        public static void CombSort(PixelSpan2D span, IComparer<TPixel> comparer)
+        {
+            int gap = span.ItemCount;
+            float shrink = 1.3f;
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                // Update the gap value for a next comb
+                gap = (int)(gap / shrink);
+                if (gap <= 1)
+                {
+                    gap = 1;
+                    sorted = true;
+                }
+                //else if (gap == 9 || gap == 10)
+                //{
+                //    gap = 11;
+                //}
+
+                for (int i = 0; i + gap < span.ItemCount; i++)
+                {
+                    if (comparer.Compare(span[i], span[i + gap]) > 0)
+                    {
+                        Swap(span, i, i + gap);
+                        sorted = false;
+                    }
+                }
+            }
+        }
+
         public static void CombSort(PixelSpan2D span, IComparer<TPixel> comparer, int pureness)
         {
             int gap = span.ItemCount;
